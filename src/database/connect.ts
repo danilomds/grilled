c7a9144ca3000de96a2ccfd2fbdb3bad741fa70b
Danilo Mendes <danilomds2007@gmail.com>
2021-01-29 05:45:02 -0300

    database connection

---
+++ src/database/connect.ts
@@ -0,0 +1,8 @@
+import { createConnection } from 'typeorm';
+
+createConnection()
+  .then(() => console.log('🗃️  Successfully connected with database'))
+  .catch(error => {
+    console.log(`Erro${error}`);
+  });
+  
